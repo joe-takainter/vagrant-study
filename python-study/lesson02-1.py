0157253185f7065609cf978dd425a5c34546af62
joe-takainter <itakeshi00@gmail.com>
2026-07-05 10:52:52 +0000

    Add lesson04

---
+++ python-study/lesson02-1.py
@@ -0,0 +1,5 @@
+name = input("お名前は？ ")
+food = input("好きな食べ物は？ ")
+
+print("こんにちは", name)
+print(food, "がお好きなんですね！")
---
+++ python-study/lesson03.py
@@ -0,0 +1,10 @@
+score = int(input("点数を入力してください："))
+
+if score >= 90:
+    print("優秀です！")
+
+elif score >= 80:
+    print("合格です！")
+
+else:
+    print("もう少し頑張りましょう！")
