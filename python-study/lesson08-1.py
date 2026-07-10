8b28647b5cb7a9126a025de5e0756080051b764c
joe-takainter <itakeshi00@gmail.com>
2026-07-10 11:18:37 +0000

    Add lesson08

---
+++ python-study/lesson08-1.py
@@ -0,0 +1,10 @@
+animals = []
+
+for i in range(3):
+    animal = input("好きな動物：")
+    animals.append(animal)
+
+print("好きな動物一覧")
+
+for animal in animals:
+    print(animal)
---
+++ python-study/lesson08.py
@@ -0,0 +1,3 @@
+fruits = ["りんご", "みかん", "バナナ"]
+
+print(fruits)
